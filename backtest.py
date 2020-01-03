1c328fea6c1a5c9aedf41ef2cd57beaccec8e871
silvercondor <32993765+silvercondor@users.noreply.github.com>
2020-01-03 15:54:08 +0800

    Create backtest.py

---
+++ backtest.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+def backtest(buy_query, sell_query, df):
+    # df should minimally have a timestamp and close column
+    df['signal'] = np.nan
+    df.loc[buy_query, 'signal'] = 1
+    df.loc[sell_query, 'signal'] = -1
+    buy_df = df.loc[df['signal'] == 1]
+    sell_df = df.loc[df['signal'] == -1]
+    reduced_df = pd.concat([sell_df, buy_df], axis=0,
+                           join='inner', ignore_index=False, copy=True)
+    reduced_df.sort_values(by=['timestamp'], inplace=True)
+    # Only take the state change
+    reduced_df.loc[(reduced_df['signal'] == 1) & 
+                   (reduced_df['signal'].shift() == 1)] = np.nan
+    reduced_df.loc[(reduced_df['signal'] == -1) &
+                   (reduced_df['signal'].shift() == -1)] = np.nan
+    reduced_df.dropna(inplace=True)
+    # Always start with buy
+    first_buy = reduced_df.loc[reduced_df['signal'] == 1].index.min()
+    reduced_df = reduced_df[first_buy:]
+    reduced_df.reset_index(drop=True, inplace=True)
+    reduced_df['delta'] = reduced_df['close']-reduced_df['close'].shift()
+    #Only count profits when selling
+    reduced_df.loc[reduced_df['signal']==1,'delta'] = np.nan
+    result = reduced_df.delta.sum()
+    return result
